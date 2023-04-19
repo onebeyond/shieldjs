06750713af72e440014f7153ffab216e2127d6c8
Ivan Esteban <ivan.esteban@one-beyond.com>
2023-04-19 13:14:01 +0200

    feat: added api types

---
+++ src/types/ApiResponse.ts
@@ -0,0 +1,9 @@
+export interface ApiResponse<T> {
+  info: {
+    count: number
+    pages: number
+    next: string | null
+    prev: string | null
+  }
+  results: T[]
+}
---
+++ src/types/index.ts
@@ -0,0 +1,2 @@
+export type { Character } from './Character'
+export type { ApiResponse } from './ApiResponse'
