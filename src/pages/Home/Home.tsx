4b253ade833bd73aa5c6d0250cd0611e667d0b96
Roberto Hernández <roberto.hernandez@guidesmiths.com>
2023-05-12 16:23:44 +0200

    feat: update home page

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,9 @@
+import { MainLayout } from '@/layout/MainLayout/MainLayout'
+
+export const Home = () => {
+  return (
+          <MainLayout>
+            <h1>Home</h1>
+          </MainLayout>
+  )
+}
