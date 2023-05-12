217859b1aa134866ea12f7b4c6c96b385b15001d
Roberto Hernández <roberto.hernandez@guidesmiths.com>
2023-05-12 18:57:28 +0200

    refactor: move menu items to array

--- src/components/NavBar/components/NavBarItems/NavBarItems.tsx
+++ src/components/NavBar/components/NavBarItems/NavBarItems.tsx
@@ -3,26 +3,21 @@ import { Link } from 'react-router-dom'
 import { StyledNavItem } from './NavBarItems.styles'
 
 export const NavBarItems: React.FC = () => {
-  return (<>
-    <StyledNavItem>
-      <Link to="/">
-        Docs
-      </Link>
-    </StyledNavItem>
-    <StyledNavItem>
-      <Link to="/">
-        Solutions
-      </Link>
-    </StyledNavItem>
-    <StyledNavItem>
-      <Link to="/fetch-example">
-        Fetch Example
-      </Link>
-    </StyledNavItem>
-    <StyledNavItem>
-      <Link to="/about">
-        About Us
-      </Link>
-    </StyledNavItem>
-  </>)
+  const menuItems = [
+    { name: 'Docs', path: '/' },
+    { name: 'Solutions', path: '/' },
+    { name: 'Fetch Example', path: '/fetch-example' },
+    { name: 'About Us', path: '/about' }
+  ]
+  return (
+    <>
+      {menuItems.map((item, index) => (
+        <StyledNavItem key={index}>
+          <Link to={item.path}>
+            {item.name}
+          </Link>
+        </StyledNavItem>
+      ))}
+    </>
+  )
 }
