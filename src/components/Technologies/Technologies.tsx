017f31eb94370df26542f43429c180827bb8ff16
Andrea Calvo <andreac.3108@gmail.com>
2023-06-01 12:44:29 +0200

    chore: move constants to file

--- src/components/Technologies/Technologies.tsx
+++ src/components/Technologies/Technologies.tsx
@@ -1,14 +1,6 @@
 import React from 'react'
-import viteLogo from '@/assets/vite.svg'
-import reactLogo from '@/assets/react.svg'
-import typescriptLogo from '@/assets/typescript.svg'
-import playwrightLogo from '@/assets/playwright.svg'
-import storybookLogo from '@/assets/storybook.svg'
-import hygenLogo from '@/assets/hygen.svg'
-import stitchesLogo from '@/assets/stitches.svg'
 import { TechnologiesSection, TechnologiesImage, TechnologiesTitle, TechnologiesSubtitle, TechnologiesTextsContainer, TechnologiesImagesContainer } from './Technologies.styles'
-
-const technologiesImages = [viteLogo, reactLogo, typescriptLogo, playwrightLogo, storybookLogo, hygenLogo, stitchesLogo]
+import { technologiesImages } from '@/constants/technologies-images'
 
 export const Technologies: React.FC = () => {
   return (
@@ -18,7 +10,7 @@ export const Technologies: React.FC = () => {
         <TechnologiesSubtitle>We provide the latest technology stack</TechnologiesSubtitle>
       </TechnologiesTextsContainer>
       <TechnologiesImagesContainer>
-        {technologiesImages.map((src: string) => <TechnologiesImage key={src} src={src} />)}
+        {technologiesImages.map(({ src, alt, key }) => <TechnologiesImage key={key} src={src} alt={alt}/>)}
       </TechnologiesImagesContainer>
     </TechnologiesSection>
   )
---
+++ src/constants/technologies-images.ts
@@ -0,0 +1,18 @@
+
+import viteLogo from '@/assets/vite.svg'
+import reactLogo from '@/assets/react.svg'
+import typescriptLogo from '@/assets/typescript.svg'
+import playwrightLogo from '@/assets/playwright.svg'
+import storybookLogo from '@/assets/storybook.svg'
+import hygenLogo from '@/assets/hygen.svg'
+import stitchesLogo from '@/assets/stitches.svg'
+
+export const technologiesImages = [
+  { src: viteLogo, alt: 'Vite Logo', key: 'viteLogo' },
+  { src: reactLogo, alt: 'React Logo', key: 'reactLogo' },
+  { src: typescriptLogo, alt: 'Typescript Logo', key: 'typescriptLogo' },
+  { src: playwrightLogo, alt: 'Playwright Logo', key: 'playwrightLogo' },
+  { src: storybookLogo, alt: 'Storybook Logo', key: 'storybookLogo' },
+  { src: hygenLogo, alt: 'Hygen Logo', key: 'hygenLogo' },
+  { src: stitchesLogo, alt: 'Stitches Logo', key: 'stitchesLogo' }
+]
