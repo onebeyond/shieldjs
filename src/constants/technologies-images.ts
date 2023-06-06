68ca493eb253b70d3a7a595f76be5f2da4ee567b
Andrea Calvo Moreno <34605171+acalvom@users.noreply.github.com>
2023-06-06 14:08:18 +0200

    Technologies component (#53)
    * feat: add Technologies component with styles
    
    * chore: move constants to file
    
    * feat: add i18n
    
    * style: add gradient
    
    * feat: add website links
    
    * test: add component test
    
    * fix: styles
    
    * chore: improve import & remove useless code

---
+++ src/constants/technologies-images.ts
@@ -0,0 +1,18 @@
+
+import viteLogo from '@/assets/vite.svg'
+import reactLogo from '@/assets/react.svg'
+import typescriptLogo from '@/assets/typescript.svg'
+import playwrightLogo from '@/assets/playwright.svg'
+import storybookLogo from '@/assets/storybook.svg'
+import hygenLogo from '@/assets/hygen.svg'
+import stitchesLogo from '@/assets/stitches.svg'
+
+export const technologiesImages = [
+  { src: viteLogo, alt: 'Vite Logo', key: 'viteLogo', href: 'https://vitejs.dev/' },
+  { src: reactLogo, alt: 'React Logo', key: 'reactLogo', href: 'https://react.dev/' },
+  { src: typescriptLogo, alt: 'Typescript Logo', key: 'typescriptLogo', href: 'https://www.typescriptlang.org/' },
+  { src: playwrightLogo, alt: 'Playwright Logo', key: 'playwrightLogo', href: 'https://playwright.dev/' },
+  { src: storybookLogo, alt: 'Storybook Logo', key: 'storybookLogo', href: 'https://storybook.js.org/' },
+  { src: hygenLogo, alt: 'Hygen Logo', key: 'hygenLogo', href: 'https://www.hygen.io/' },
+  { src: stitchesLogo, alt: 'Stitches Logo', key: 'stitchesLogo', href: 'https://stitches.dev/' }
+]
