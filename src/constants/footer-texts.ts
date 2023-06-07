a6e7bb4e38ec2d5f883a6cebde95de1e372c324c
roberto <roberto.hernandez@one-beyond.com>
2023-06-07 13:46:01 +0200

    feat: fix footer translation

--- src/constants/footer-texts.ts
+++ src/constants/footer-texts.ts
@@ -1,6 +1,6 @@
 export const footerTitleES = 'Shield.js'
-export const oneBeyondSubtitleES = 'Desde 2023 - One Beyond'
+export const oneBeyondSubtitleES = 'Desde 2022 - One Beyond'
 export const contactUsTitleES = 'Contacta con nosotros'
 export const footerTitleEN = 'Shield.js'
-export const oneBeyondSubtitleEN = 'Since 2023 - One Beyond'
+export const oneBeyondSubtitleEN = 'Since 2022 - One Beyond'
 export const contactUsTitleEN = 'Contact us'
