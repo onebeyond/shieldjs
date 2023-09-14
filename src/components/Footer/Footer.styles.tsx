9c32de74b2eeaf69aa7fb4a659b8ab34692edf36
jgleal <jgleal@gmail.com>
2023-09-14 12:34:32 +0200

    feat: footer (WIP)

--- src/components/Footer/Footer.styles.tsx
+++ src/components/Footer/Footer.styles.tsx
@@ -1,91 +1,91 @@
-import { styled } from '@/stitches.config'
+import { styled } from 'styled-components'
 
-export const FooterWrapper = styled('footer', {
-  backgroundColor: 'transparent',
-  borderTop: '1px solid #ddd',
-  color: '#fff',
-  mx: '$6',
-  py: '$2',
-  '@md': {
-    mx: '$7',
-    py: '$5'
+export const FooterWrapper = styled.footer`
+  background-color: transparent;
+  border-top: 1px solid #ddd;
+  color: #fff;
+  margin: 0 2rem;
+  padding: 0.5rem 0;
+  ${props => props.theme.mediaQueries.md} {
+    margin: 0 3rem;
+    padding: 1.5rem 0
+  };
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  justify-content: space-between;
+  margin-top: auto;
+`
+
+export const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  gap: 1rem;
+  ${props => props.theme.mediaQueries.md} {
+    gap: 1.5rem
+  }
+`
+
+export const LeftColumn = styled(Column)`
+  align-items: flex-start;
+`
+
+export const RightColumn = styled(Column)`
+  align-items: flex-end
+`
+
+export const MenuList = styled.ul`
+  p: 0, // Add to reset.css
+  m: 0, // Add to reset.css
+  display: flex,
+  justifyContent: space-around,
+  alignItems: center,
+  gap: $5,
+  @md: {
+    gap: 3.8rem
+  }
+`
+
+export const MenuListItem = styled.li`
+  display: flex,
+  alignItems: center,
+  justifyContent: center,
+  mb: $4
+`
+
+export const Subtitle = styled.h3`
+  color: #fff,
+  p: 0, // Add to reset.css
+  m: 0, // Add to reset.css
+  fontSize: $4,
+  @md: {
+    fontSize: $6
   },
-  display: 'grid',
-  gridTemplateColumns: 'repeat(2, 1fr)',
-  justifyContent: 'space-between',
-  mt: 'auto'
-})
+  textAlign: left
+`
 
-export const Column = styled('div', {
-  display: 'flex',
-  flexDirection: 'column',
-  width: '100%',
-  gap: '$4',
-  '@md': {
-    gap: '$5'
-  }
-})
-
-export const LeftColumn = styled(Column, {
-  alignItems: 'flex-start'
-})
-
-export const RightColumn = styled(Column, {
-  alignItems: 'flex-end'
-})
-
-export const MenuList = styled('ul', {
-  p: '0', // Add to reset.css
-  m: '0', // Add to reset.css
-  display: 'flex',
-  justifyContent: 'space-around',
-  alignItems: 'center',
-  gap: '$5',
-  '@md': {
-    gap: '3.8rem'
-  }
-})
-
-export const MenuListItem = styled('li', {
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  mb: '$4'
-})
-
-export const Subtitle = styled('h3', {
-  color: '#fff',
-  p: '0', // Add to reset.css
-  m: '0', // Add to reset.css
-  fontSize: '$4',
-  '@md': {
-    fontSize: '$6'
+export const Title = styled.h2`
+  color: #fff,
+  fontSize: $5,
+  @md: {
+    fontSize: $7
   },
-  textAlign: 'left'
-})
+  m: 0,
+  p: 0,
+  textAlign: left
 
-export const Title = styled('h2', {
-  color: '#fff',
-  fontSize: '$5',
-  '@md': {
-    fontSize: '$7'
-  },
-  m: '0',
-  p: '0',
-  textAlign: 'left'
+`
 
-})
-
-export const OneBeyondIcon = styled('img', {
-  height: '$8',
-  '@md': {
-    height: '$9'
+export const OneBeyondIcon = styled.img`
+  height: $8,
+  @md: {
+    height: $9
   }
-})
+`
 
-export const LinkIcon = styled('img', {
-  height: '2rem',
-  '@md': {
-    height: '2.7rem'
+export const LinkIcon = styled.img`
+  height: 2rem,
+  @md: {
+    height: 2.7rem
   }
-})
+`
