534b6be27a372c96a092f541367e8b5d6dda4dcb
jgleal <jgleal@gmail.com>
2023-09-14 11:55:38 +0200

    feat: migrate hero styles

--- src/components/Hero/Hero.styles.tsx
+++ src/components/Hero/Hero.styles.tsx
@@ -1,70 +1,66 @@
-import { styled } from '@/stitches.config'
+import { styled } from 'styled-components'
 
-export const HeroSection = styled('section', {
-  display: 'flex',
-  flexGrow: 1,
-  justifyContent: 'center',
-  alignItems: 'center',
-  background: '$gradientBlueToGray',
-  px: '2rem',
-  py: '3rem',
-  '@md': {
-    px: '6rem',
-    py: '4.5rem'
+export const HeroSection = styled.section`
+  display: 'flex';
+  flex-grow: 1;
+  justify-content: 'center';
+  align-items: 'center';
+  background: ${props => props.theme.colors.gradientBlueToGray};
+  padding: '3rem 2rem';
+  ${props => props.theme.mediaQueries.md} {
+    padding: '4.5rem 6rem';
   }
-})
+`
 
-export const HeroContent = styled('div', {
-  display: 'flex',
-  flexDirection: 'column',
-  flexBasis: '100%',
-  textAlign: 'left',
-  '@md': {
-    flexBasis: '40%'
+export const HeroContent = styled.div`
+  display: 'flex';
+  flex-direction: 'column';
+  flex-basis: '100%';
+  text-align: 'left';
+  ${props => props.theme.mediaQueries.md} {
+    flex-basis: '40%'
   }
-})
+`
 
-export const HeroTitle = styled('h1', {
-  fontSize: '$8',
-  my: 0,
-  '@md': {
-    fontSize: '$9'
+export const HeroTitle = styled.h1`
+  font-size: ${props => props.theme.fonSizes.xl};
+  margin: '0 auto'
+  ${props => props.theme.mediaQueries.md} {
+    font-size: ${props => props.theme.fonSizes.xxl};
   }
+`
 
-})
-
-export const HeroSubtitle = styled('h2', {
-  fontSize: '$6',
-  lineHeight: '1.1',
-  fontWeight: 'normal',
-  '@md': {
-    fontSize: '$7'
+export const HeroSubtitle = styled.h2`
+  font-size: ${props => props.theme.fonSizes.lg};
+  line-height: '1.1';
+  font-weight: 'normal';
+  ${props => props.theme.mediaQueries.md} {
+    font-size: ${props => props.theme.fonSizes.xl};
   }
-})
+`
 
-export const HeroDescription = styled('p', {
-  fontSize: '$4',
-  '@md': {
-    fontSize: '$5'
+export const HeroDescription = styled.p`
+  font-size: ${props => props.theme.fonSizes.xl};
+  ${props => props.theme.mediaQueries.md} {
+    font-size: ${props => props.theme.fonSizes.lg};
   }
+`
 
-})
-
-export const HeroImage = styled('img', {
-  display: 'none',
-  height: '180px',
-  flexBasis: '30%',
-  '@md': {
+export const HeroImage = styled.img`
+  display: 'none';
+  height: '180px';
+  flex-basis: '30%';
+  ${props => props.theme.mediaQueries.md} {
     display: 'block'
   }
-})
+`
 
-export const HeroButton = styled('button', {
-  mx: 'auto',
-  width: 'max-content',
-  color: 'black',
-  borderRadius: '40px',
-  border: '1px solid rgb(97, 218, 251)',
-  padding: '0.5rem 2rem',
-  background: '$gradientButton'
-})
+export const HeroButton = styled.button`
+  margin: 'auto';
+  width: 'max-content';
+  color: 'black';
+  border-radius: '40px';
+  border: '1px solid rgb(97; 218; 251)';
+  padding: '0.5rem 2rem';
+  background: ${props => props.theme.colors.gradientButton};
+`
