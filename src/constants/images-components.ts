2d868b08d3f4a754d67e2d08cf8282bace98b3e0
Andrea Calvo <andreac.3108@gmail.com>
2023-11-14 09:13:31 +0100

    fix: remove storybook logo

--- src/constants/images-components.ts
+++ src/constants/images-components.ts
@@ -2,7 +2,6 @@ import viteLogo from '@/assets/vite.svg'
 import reactLogo from '@/assets/react.svg'
 import typescriptLogo from '@/assets/typescript.svg'
 import playwrightLogo from '@/assets/playwright.svg'
-import storybookLogo from '@/assets/storybook.svg'
 import hygenLogo from '@/assets/hygen.svg'
 import stitchesLogo from '@/assets/stitches.svg'
 import tutorialGraph from '@/assets/tutorial-graph.svg'
@@ -12,7 +11,6 @@ export const technologiesImages = [
   { src: reactLogo, alt: 'React Logo', key: 'reactLogo', href: 'https://react.dev/' },
   { src: typescriptLogo, alt: 'Typescript Logo', key: 'typescriptLogo', href: 'https://www.typescriptlang.org/' },
   { src: playwrightLogo, alt: 'Playwright Logo', key: 'playwrightLogo', href: 'https://playwright.dev/' },
-  { src: storybookLogo, alt: 'Storybook Logo', key: 'storybookLogo', href: 'https://storybook.js.org/' },
   { src: hygenLogo, alt: 'Hygen Logo', key: 'hygenLogo', href: 'https://www.hygen.io/' },
   { src: stitchesLogo, alt: 'Stitches Logo', key: 'stitchesLogo', href: 'https://stitches.dev/' }
 ]
