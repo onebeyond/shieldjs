84f1d7e67a26e5a2bc1faebaaa862b4976b95384
Andrea Calvo <andreac.3108@gmail.com>
2023-11-15 17:31:24 +0100

    fix: test configuration after breaking changes

--- src/components/Hero/Hero.test.tsx
+++ src/components/Hero/Hero.test.tsx
@@ -2,15 +2,18 @@ import { render, screen } from '@testing-library/react'
 import { expect, test } from 'vitest'
 
 import { Hero } from './Hero'
+import theme from '@/styled.config'
+import { ThemeProvider } from 'styled-components'
 
 test('renders Hero component', () => {
   render(
-    <Hero />
+    <ThemeProvider theme={theme}>
+      <Hero />
+    </ThemeProvider>
   )
   expect(screen.getByText('Shield.js')).toBeInTheDocument()
   expect(screen.getByText('The new frontend project by One Beyond')).toBeInTheDocument()
   expect(screen.getByText('From developers, to developers. Make the beginning easier with all daytools installed in one place.')).toBeInTheDocument()
   expect(screen.getByText('Start now')).toBeInTheDocument()
-  expect(screen.getByRole('img')).toBeInTheDocument()
   expect(screen.getByAltText('ShieldJS logo')).toBeInTheDocument()
 })
--- src/components/ReadMore/ReadMore.test.tsx
+++ src/components/ReadMore/ReadMore.test.tsx
@@ -2,9 +2,15 @@ import { render, screen } from '@testing-library/react'
 import { expect, test } from 'vitest'
 
 import { ReadMore } from './ReadMore'
+import theme from '@/styled.config'
+import { ThemeProvider } from 'styled-components'
 
 test('renders Read More', () => {
-  render(<ReadMore />)
+  render(
+    <ThemeProvider theme={theme}>
+      <ReadMore />
+   </ThemeProvider>
+  )
   const titleElement = screen.getByText(/Read More/i)
   expect(titleElement).toBeInTheDocument()
 })
