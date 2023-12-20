4ff11324742def712410d2e4cb5384484e30b9da
jgleal <jgleal@gmail.com>
2023-12-20 13:06:17 +0100

    fix: button inteface

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 import { StyledButton } from './Button.styles'
 
-export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  size?: 'small' | 'large'
+export interface ButtonProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
+  size?: 'small' | 'large';
 }
 
-export const Button: React.FC<ButtonProps> = ({ size, disabled, children, onClick, ...props }) => {
+export const Button: React.FC<ButtonProps> = ({ size, children, onClick, ...props }) => {
   return (
-      <StyledButton type="button" onClick={onClick} disabled={disabled} size={size} {...props}>
+      <StyledButton type="button" onClick={onClick} {...props}>
           {children}
       </StyledButton>
   )
