82be3f13ea1c2c062ec033ae37cc2f94c8972b29
Roberto Hernández <58053533+robertoHeCi@users.noreply.github.com>
2023-12-21 11:43:23 +0100

    Refactor/about page (#134)
    * feat: add shield images with authors
    
    * feat: add shield images with authors in the about page

--- src/pages/About/About.styles.tsx
+++ src/pages/About/About.styles.tsx
@@ -7,3 +7,19 @@ export const AboutWrapper = styled.div`
   flex-direction: column;
   margin: 0 auto;
 `
+export const StyledImageList = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+  gap: 1rem;
+  img {
+    width: auto;
+    height: 200px;
+    transition: transform 1s ease-out;
+  &:hover {
+    transform: scale(1.2);
+  }
+  }
+`
